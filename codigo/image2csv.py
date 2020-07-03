ecbea2f0c40414c3b359ef1e1e767e67ee0f178e
lucas-koiti <lucaskoitig@gmail.com>
2020-07-03 01:40:41 +0000

    planilha com os dados

--- codigo/image2csv.py
+++ codigo/image2csv.py
@@ -1,13 +1,18 @@
+"""
+    Criação da planilha apenas com algumas imagens, pois a prefeitura mudou de padrao para divulgação 3 vezes.
+    Alguns erros de identificação pelo pytesseract corrigidos manualmente. 
+"""
 import os
+import csv
 import pytesseract
-
 from PIL import Image
+
 
 # configuracao para o tesseract identificar numeros
 custom_config = r'--oem 3 --psm 8 outputbase digits'
 
 # linhas da planilha
-csvrow = [['Dia', 'Confirmados', 'Obitos', 'Recuperacao', 'Recuperados']]
+csvrows = []
 
 # percorre cada imagem da pasta /dados/imagens/tipo2
 for entry in os.scandir(r'../dados/imagens/tipo2'):
@@ -40,10 +45,17 @@ for entry in os.scandir(r'../dados/imagens/tipo2'):
         recuperados = image.crop((left,top,right,bottom))
         rec = (pytesseract.image_to_string(recuperados, lang='por', config=custom_config))
         
-        dia = entry.path[-6:-4]+'/'+entry.path[-7:-6]
+        dia = int(entry.path[-7:-4])
         row = [dia, conf, obt, inter, 0, rec]
 
-        csvrow.append(row.copy())
+        csvrows.append(row.copy())
 
-for i in csvrow:
-    print(i)
+# ordena a lista de linhas pela data, pois o os.scandir itera de forma arbitraria
+csvrows.sort(key=lambda x: x[0])
+csvrows.insert(0, ['Dia', 'Confirmados', 'Obitos', 'Recuperacao', 'Recuperados'])
+
+# cria a planilha na pasta dados/planilha_csv
+with open('../dados/planilha_csv/dados.csv', 'w', newline='') as planilha:
+    wr = csv.writer(planilha, quoting = csv.QUOTE_ALL)
+    for row in csvrows:
+        wr.writerow(row)
