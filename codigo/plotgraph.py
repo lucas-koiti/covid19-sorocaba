95c07e9daf8e15e1878cba46e1d0edd04644c12a
lucas-koiti <lucaskoitig@gmail.com>
2020-08-07 17:56:04 +0000

    dados atualizados 07/08

--- codigo/plotgraph.py
+++ codigo/plotgraph.py
@@ -4,7 +4,7 @@ import plotly.express as px
 dataf = pd.read_csv('../dados/planilha_csv/dados.csv')
 
 fig = px.line(dataf, x = 'Dia', y = 'Confirmados', title='Covid-19 Casos Confirmados')
-fig.show()
+#fig.show()
 
 fig = px.line(dataf, x = 'Dia', y = 'Obitos', title='Covid-19 Óbitos em Sorocaba')
-fig.show()
+#fig.show()
